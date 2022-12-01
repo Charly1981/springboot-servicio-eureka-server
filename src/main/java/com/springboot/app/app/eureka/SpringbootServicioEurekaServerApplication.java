6de3cff518601514bac06e441dac61ef0af04d24
Carlos Zirpoli <ZIRPOLIC@redlink.com.ar>
2022-12-01 13:08:02 -0300

    Test git

--- src/main/java/com/springboot/app/app/eureka/SpringbootServicioEurekaServerApplication.java
+++ src/main/java/com/springboot/app/app/eureka/SpringbootServicioEurekaServerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class SpringbootServicioEurekaServerApplication {
 
+	// test Git Link
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootServicioEurekaServerApplication.class, args);
 	}
